c3fe3204d544fd1dce1a525650023bb3f8c03562
sevru <sevruliza@gmail.com>
2025-11-26 01:16:47 +1000

    Add project files.

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void onButtonStartClick(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).Content = (sender as Button).Content == "Stop" ? "Start" : "Stop";
+            //btnStart.Content = btnStart.Content == "Stop" ? "Start" : "Stop";
+            //MessageBox.Show("Hello, WPF!");
+        }
+    }
+}
