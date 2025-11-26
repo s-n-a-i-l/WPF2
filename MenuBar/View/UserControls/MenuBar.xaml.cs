a6d9c7bc3ea952c6f074f4eff017c2b7851ab675
sevru <sevruliza@gmail.com>
2025-11-26 22:27:15 +1000

    Reusable MenuBar

---
+++ MenuBar/View/UserControls/MenuBar.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MenuBar.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MenuBar.xaml
+    /// </summary>
+    public partial class MenuBar : UserControl
+    {
+        public MenuBar()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Window WindowParent = Window.GetWindow(this);
+            WindowParent.Close();
+        }
+    }
+}
