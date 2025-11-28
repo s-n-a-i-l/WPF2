43cb0ceef95fe775af5c3a9433c1d5e5111e7c33
sevru <sevruliza@gmail.com>
2025-11-28 21:40:51 +1000

    Data Binding DONE;

--- Bimding/MainWindow.xaml.cs
+++ Bimding/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,32 +13,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.CompilerServices;
 
 namespace Bimding
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
+            DataContext = this;
             InitializeComponent();
+            txtInput.Focus();
         }
-
-        private void btnSet_Click(object sender, RoutedEventArgs e)
-        {
-            if(!string.IsNullOrWhiteSpace(txtInput.Text)) 
-            tbResult.Text = txtInput.Text;
-        }
+        //private void btnSet_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if(!string.IsNullOrWhiteSpace(txtInput.Text)) 
+        //    tbResult.Text = txtInput.Text;
+        //}
+        public event PropertyChangedEventHandler PropertyChanged;
 
         private string bounText;
 
         public string BoundText
         {
             get { return bounText; }
-            set { bounText = value; }
+            set 
+            { 
+                bounText = value;
+                OnPropertyChanged();
+                //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(BoundText))); 
+            }
+        }
+        void OnPropertyChanged([CallerMemberName] string propertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from button";
+        }
     }
 }
