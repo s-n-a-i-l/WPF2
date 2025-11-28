bb40bf6bcc6024d3e31f5a3fc49632d7eafb7d5c
sevru <sevruliza@gmail.com>
2025-11-28 23:24:15 +1000

    list box delete DONE;

---
+++ Dialogs/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using WinForms = System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Dialogs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMessageBox_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("This is MessageBox", "Caption", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+           OpenFileDialog dialog = new OpenFileDialog();
+           dialog.ShowDialog();
+           if(!string.IsNullOrWhiteSpace(dialog.FileName)) 
+           {
+                MessageBox.Show(dialog.FileName, "Filename", MessageBoxButton.OK, MessageBoxImage.Information);
+           }
+        }
+
+        private void btnOpenFolder_Click(object sender, RoutedEventArgs e)
+        {
+            WinForms.FolderBrowserDialog dialog = new WinForms.FolderBrowserDialog();
+            dialog.ShowDialog();
+            MessageBox.Show(dialog.SelectedPath, "Folder", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ ListBox/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Directory.SetCurrentDirectory("C:\\");
+
+            string[] files = Directory.EnumerateFiles("C:\\Windows").ToArray();
+
+            for (int i = 0; i < files.Length; i++) 
+            {
+              listBox.Items.Add(files[i].Split('\\').Last());
+            }
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            if(!listBox.Items.Contains(txtInput.Text))
+            listBox.Items.Add(txtInput.Text);
+        }
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            //listBox.Items.Remove(listBox.SelectedItem);
+            //System.Collections.IList files = new  List(listBox.SelectedItems);
+            //foreach (object i in files) 
+            //{
+            //    listBox.Items.Remove(i);
+            //}
+            while (listBox.SelectedItems.Count > 0) listBox.Items.Remove(listBox.SelectedItem);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+        }
+
+    }
+}
