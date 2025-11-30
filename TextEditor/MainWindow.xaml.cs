9266738ce9e0d127a36fc1974744fccc2c99db2c
sevru <sevruliza@gmail.com>
2025-12-01 00:09:56 +1000

    dz in procces

---
+++ TextEditor/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WinForms = System.Windows.Forms;
+
+namespace TextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+                System.Windows.Forms.SaveFileDialog saveFile1 = new System.Windows.Forms.SaveFileDialog();
+                saveFile1.DefaultExt = "*.txt";
+                saveFile1.Filter = "Text files|*.txt";
+            if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
+                saveFile1.FileName.Length > 0)
+            {
+                try
+                {
+                    string textToSave = txtBox1.Text;
+
+                    File.WriteAllText(saveFile1.FileName, textToSave);
+                    System.Windows.Forms.MessageBox.Show("Текст сохранен успешно!");
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show($"Ошибка сохранения: {ex.Message}");
+                }
+            }
+        }
+    }
+}
